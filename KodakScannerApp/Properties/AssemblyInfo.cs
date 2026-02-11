f9da93953bbdc89928acc995962fa30f3668f7ad
DWANNER1 <114098810+DWANNER1@users.noreply.github.com>
2026-02-11 14:41:53 -0500

    Bump version to 2.0.0 for release

--- KodakScannerApp/Properties/AssemblyInfo.cs
+++ KodakScannerApp/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.3.21.0")]
-[assembly: AssemblyFileVersion("1.3.21.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
